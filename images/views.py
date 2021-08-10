40ad0f50da764aecac6512dceca9262fac04570d
abdula <kajnazarov.com>
2021-08-10 16:44:43 +0500

    Set DEBUG based on environment variables.

--- images/views.py
+++ images/views.py
@@ -84,7 +84,7 @@ def image_like(request):
 @login_required
 def image_list(request):
     images = Image.objects.all()
-    paginator = Paginator(images, 8)
+    paginator = Paginator(images, 15)
     page = request.GET.get('page')
     try:
         images = paginator.page(page)
